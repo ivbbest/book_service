3f54b783e4db626ee461ea965a0a19ca3b8f3e4c
Vladimir Irkhin <ivb1989@yandex.ru>
2023-12-06 21:56:56 +0300

    Add migrations file alembic. Create table. Update requirements. Modified schemas.

---
+++ models/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP
+
+metadata = MetaData()
+
+users = Table(
+    "user",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("email", String, nullable=False),
+    Column("username", String, nullable=False),
+    Column("password", String, nullable=False),
+    Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+)
